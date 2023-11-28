4e9f9cd0e12b984f218aeb4cae4d73fffbd4400c
Amanda Medeiros <amandasmedeiros@outlook.com.br>
2023-11-28 10:41:32 -0300

    cr: Timeline | cr: Contact

--- bibibranco/src/components/Contact/Contact.jsx
+++ bibibranco/src/components/Contact/Contact.jsx
@@ -10,20 +10,20 @@ function Contact() {
     <>
 
     <div className='wrapGetInTouch'>
-    <div className='containerGetInTouch'>
+      <div className='containerGetInTouch'>
 
-    <SectionTitle type="secondary" text="get in touch">
-        </SectionTitle>
+      <SectionTitle type="secondary" text="get in touch">
+          </SectionTitle>
 
-      <p>say hi, invite me to collaborate on a project or just ask for my PSN so I can beat you on EAFC 24</p>
+        <p>say hi, invite me to collaborate on a project or just ask for my PSN so I can beat you on EAFC 24</p>
 
-      <div className='linkFooter'>
-        <a href='https://www.linkedin.com/in/bibibranco/' target='_blank'>linkedin<ArrowSquareOut size={24}/></a> 
-        <a href='mailto: oi@bibibran.co' target='_blank'>oi@bibibran.co<At size={24}/> </a>
+        <div className='linkFooter'>
+          <a href='https://www.linkedin.com/in/bibibranco/' target='_blank'>linkedin<ArrowSquareOut size={24}/></a> 
+          <a href='mailto: oi@bibibran.co' target='_blank'>oi@bibibran.co<At size={24}/> </a>
+
+        </div>
 
       </div>
-
-    </div>
     </div>
     </>
   )
