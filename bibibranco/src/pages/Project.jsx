c27dfb147864492003f9473a87fb713d56c168f8
Bibiana Branco <brancobibiana@gmail.com>
2023-12-01 10:04:34 -0300

    nem lembor mais

--- bibibranco/src/pages/Project.jsx
+++ bibibranco/src/pages/Project.jsx
@@ -7,8 +7,8 @@ import { useParams } from 'react-router-dom';
 let projetos = [
     {
         id:1,
-        nome: 'Product Design', 
-        description:'I’ve been working with end-to-end user experience design for the last four years, creating meaningful blablabla for global companies such as Itau Unibanco, Melhor Envio, AEDIT',
+        nome: 'product design', 
+        description:'I’ve been working with end-to-end user experience design for the last five years, creating meaningful blablabla for global companies such as Itau Unibanco, Melhor Envio, AEDIT',
         content:[
             {
                 imgUrl: '../img01.jpg',
@@ -20,13 +20,13 @@ let projetos = [
                 imgDesc: 'desc2',
                 imgTxt: `Lorem Ipsum ainda é is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
 
-            },
+            }
     ]
     },
     {
         id:2,
-        nome: 'Crafting', 
-        description:'muita cerveja e coca cola zero envolvidathe last four years, creating meaningful blablabla for global companies such as Itau Unibanco, Melhor Envio, AEDIT',
+        nome: 'crafting', 
+        description:'woodworking, cutting glass, graphic design and a pile of different hobbies collected throughout the years',
         content:[{
             imgUrl: '../img01.jpg',
             imgDesc: 'abuble',
