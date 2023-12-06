e8516e259d099e8886ede5e560055775f25169bc
Bibiana Branco <brancobibiana@gmail.com>
2023-12-06 12:30:13 -0300

    image content height fix

--- bibibranco/src/pages/ProjectContent/ProjectContent.jsx
+++ bibibranco/src/pages/ProjectContent/ProjectContent.jsx
@@ -4,7 +4,7 @@ import "./ProjectContent.css"
 function ProjectContent(props){
     return(
         <div className="project-content">
-            <div style={{ backgroundImage: `url(${props.imgUrl})` }} className="project-content__img"></div>
+            <div style={{ backgroundImage: `url(${props.imgUrl})`,height:`${props.imgUrl==undefined? 'auto':'400px'}` }} className="project-content__img"></div>
             <p className="project-content__img-desc">{props.imgDesc}</p>
             <p className="project-content__text">{props.imgTxt}</p>
         </div>
